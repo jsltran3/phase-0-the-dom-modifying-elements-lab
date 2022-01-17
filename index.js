beab77d562d88544a8dee7c637d12690b993706c
Joseph <jsltran@gmail.com>
2022-01-17 15:43:14 -0800

    commit tree fdb90b96a3ea45630f59e67e95bed417886d85b7

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// // Write your code here!
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1"); 
+
+newHeader.id = 'victory';
+
+// newHeader.innerHTML = 'YOUR-NAMEs is the Champion';
+
+newHeader.innerHTML = "YOUR-NAME is the champion!";
